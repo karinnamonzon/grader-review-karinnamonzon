804f69de1c864df6a3797a72fa143ab14b99d1c6
karinnamonzon <kmonzon@ucsd.edu>
2023-02-16 12:34:27 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -3,3 +3,25 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+if[[-f GradeServer.java]]
+then 
+    echo "GradeServer found"
+else
+    echo "need GradeServer"
+    exit 1
+fi
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+
+if[[$? -neq 0]]
+then 
+    echo "error"
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+
+set -e
+
+

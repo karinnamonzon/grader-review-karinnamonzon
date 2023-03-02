ea65fe2d3b28919c9671916a758d1d6bf10dac54
karinnamonzon <114766426+karinnamonzon@users.noreply.github.com>
2023-03-02 12:25:42 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,27 +1,45 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar"
 
 rm -rf student-submission
+
 git clone $1 student-submission
-echo 'Finished cloning'
+echo 'Finished cloning student-submission'
+
+if [[ -f "student-submission/ListExamples.java" ]]
+then
+    echo 'ListExamples.java found'
+else
+    echo 'ListExamples.java not found'
+    exit 1
+fi 
+
+cp TestListExamples.java student-submission/
 
 cd student-submission
-if[[-f GradeServer.java]]
-then 
-    echo "GradeServer found"
+
+javac ListExamples.java
+if [[ -f "StringChecker.class" ]] && [[ -f "ListExamples.class" ]]
+then
+    echo "SUCCESSFUL COMPILE"
 else
-    echo "need GradeServer"
-    exit 1
+    echo "FAILURE TO COMPILE"
+    exit 2
 fi
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-
-if[[$? -neq 0]]
-then 
-    echo "error"
-fi
-
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
-
-set -e
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
+java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
 
 
+java -cp $CPATH org.junit.runner.JUnitCore student-submission/*.java > output.txt
+
+grep "Tests run" output.txt > grepResults.txt
+
+TESTSRUN=` cut -d ',' -f 1 grepResults.txt `
+
+FAILURES=` cut -d ',' -f 2 grepResults.txt `
+
+NUMFAILURES="${FAILURES//[^0-9]/}"
+echo $NUMFAILURES
+
+NUMTESTS="${TESTSRUN//[^0-9]/}"
+echo $NUMTESTS
